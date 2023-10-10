145d1bd148b0d98f5a98590e8dc819c61c0a7c29
kirryl <kirryltarunin@outlook.com>
2023-10-10 23:26:46 +0100

    Add bootstrap

--- data-kernel/src/components/data-export.js
+++ data-kernel/src/components/data-export.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function dataExport({}) {
-    return <div>Hello World!</div>
+
+
+    return (
+        <React.Fragment>
+            <h1 primary>Nations Data</h1>
+            <Button btn-primary>Export JSON</Button>
+        </React.Fragment>
+    ) 
 }
 
 export default dataExport;
