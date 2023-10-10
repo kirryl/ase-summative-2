766f3f6d5016e037a88d64df785842441fb00106
kirryl <kirryltarunin@outlook.com>
2023-10-11 00:24:52 +0100

    Create export page and add stylesheet

--- data-kernel/src/components/data-export.js
+++ data-kernel/src/components/data-export.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-function dataExport({}) {
-
+function dataExport() {
+    function handleClickExport(e) {
+        e.preventDefault();
+        console.log('You clicked export.');
+      }
 
     return (
         <React.Fragment>
-            <h1 primary>Nations Data</h1>
-            <Button btn-primary>Export JSON</Button>
+            <h1 className="text-primary">Nations Data</h1>
+            <body>This is a portal for exporting country data</body>
+            <Button className="btn-primary" onClick={handleClickExport}>Export JSON</Button>
         </React.Fragment>
     ) 
 }
