9929f017f13e141edbce25f6a236263a492065a4
kirryl <kirryltarunin@outlook.com>
2023-10-11 18:51:07 +0100

    Add api function

--- data-kernel/src/components/data-export.js
+++ data-kernel/src/components/data-export.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import { useState } from "react";
 
 function DataExport() {
-    function handleClickExport(e) {
-        e.preventDefault();
-        console.log('You clicked export.');
-      }
+  const [data, setData] = useState(null);
 
-    return (
-        <React.Fragment>
-            <h1 data-testid="page-title" className="text-primary">Nations Data</h1>
-            <text data-testid="page-description" className="text-secondary">This is a portal for exporting country data</text>
-            <Button data-testid="page-export-button" className="btn-primary" onClick={handleClickExport}>Export JSON</Button>
-        </React.Fragment>
-    ) 
+  const handleClickExport = async () => {
+    try {
+      const data = await (
+        await fetch(`https://restcountries.com/v3.1/all`)
+      ).json();
+      setData(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <Container fluid>
+      <h1 data-testid="page-title" className="text-primary">
+        Nations Data
+      </h1>
+      <div>
+        <p data-testid="page-description" className="text-secondary">
+          This is a portal for exporting country data
+        </p>
+      </div>
+      <Button
+        type="submit"
+        data-testid="page-export-button"
+        className="btn-primary"
+        onClick={handleClickExport}
+      >
+        Export JSON
+      </Button>
+    </Container>
+  );
 }
 
 export default DataExport;
