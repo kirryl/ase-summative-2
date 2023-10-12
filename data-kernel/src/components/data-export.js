c3085a120a6881aed85bcba63c4d578d91a293fc
kirryl <kirryltarunin@outlook.com>
2023-10-12 12:13:33 +0100

    Add untested axios hook

--- data-kernel/src/components/data-export.js
+++ data-kernel/src/components/data-export.js
@@ -1,22 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import useLoadCountries from "../hooks/use-countries-api";
 import { useState } from "react";
 
 function DataExport() {
-  const [data, setData] = useState(null);
-
-  const handleClickExport = async () => {
-    try {
-      const data = await (
-        await fetch(`https://restcountries.com/v3.1/all`)
-      ).json();
-      setData(data);
-      console.log(data);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+  const { data, loading, error, getData } = useLoadCountries();
 
   return (
     <Container fluid>
@@ -32,7 +21,7 @@ function DataExport() {
         type="submit"
         data-testid="page-export-button"
         className="btn-primary"
-        onClick={handleClickExport}
+        onClick={() => getData}
       >
         Export JSON
       </Button>
